0983f073ab89764e923927e4e1781085cf228080
KyawMinHtway066813 <batman066813@gmail.com>
2024-02-15 03:40:08 +0630

    second commit

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ app.use(express.json());
 
 const posts = [
     {
-        username: 'Kyaw Min Htway',
-        title: 'Post 1'
+        username: "Kyaw Min Htway",
+        title: "King"
     },
     {
-        username: 'Khin Myat Mon Kyaw',
-        title: 'Post 2'
+        username: "Khin Myat Mon Kyaw",
+        title: "Queen"
     }
 ]
-app.get('/posts', (req, res) => {
-    res.json(posts)
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
 })
 
 app.post('/login', (req,res) => {
@@ -29,5 +29,17 @@ app.post('/login', (req,res) => {
    const accessToken =  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
    res.json({ accessToken: accessToken })
 })
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.header['authorization']
+    const token = authHeader && authHeader.split('')[1]
+    if(token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
 
 app.listen(3000);
